c63ebe468620a55fbf2417445be88ab8c3b7a265
Ganesh Maurya <gvgmaurya@gmail.com>
2020-08-31 23:23:57 +0530

    hello world

--- app.js
+++ app.js
@@ -2,6 +2,15 @@ var express = require("express");
 var app = express();
 var bodyParser = require("body-parser");
 var mongoose = require("mongoose");
+const PORT = 3000;
 
 app.use(bodyParser.urlencoded({extenden : true}));
 app.set("view engine", "ejs");
+
+app.get("/", function(req, res){
+    res.render();
+})
+
+app.listen(PORT, function(){
+    console.log("Welcome to my blog app server.");
+})
