4e792ce6eea5818672e93f6270718b910f9e449d
G V Ganesh Maurya <gvgmaurya@gmail.com>
2020-09-22 13:16:49 +0530

    add cards for posts

--- app.js
+++ app.js
@@ -4,6 +4,18 @@ var bodyParser = require("body-parser");
 var mongoose = require("mongoose");
 const PORT = 3000;
 
+var posts = [
+    {name: "Salmon Creek", image: "https://farm9.staticflickr.com/8442/7962474612_bf2baf67c0.jpg"},
+    {name: "Granite Hill", image: "https://farm1.staticflickr.com/60/215827008_6489cd30c3.jpg"},
+    {name: "Mountain Goat's Rest", image: "https://farm7.staticflickr.com/6057/6234565071_4d20668bbd.jpg"},
+    {name: "Salmon Creek", image: "https://farm9.staticflickr.com/8442/7962474612_bf2baf67c0.jpg"},
+    {name: "Granite Hill", image: "https://farm1.staticflickr.com/60/215827008_6489cd30c3.jpg"},
+    {name: "Mountain Goat's Rest", image: "https://farm7.staticflickr.com/6057/6234565071_4d20668bbd.jpg"},
+    {name: "Salmon Creek", image: "https://farm9.staticflickr.com/8442/7962474612_bf2baf67c0.jpg"},
+    {name: "Granite Hill", image: "https://farm1.staticflickr.com/60/215827008_6489cd30c3.jpg"},
+    {name: "Mountain Goat's Rest", image: "https://farm7.staticflickr.com/6057/6234565071_4d20668bbd.jpg"}
+];
+
 app.use(bodyParser.urlencoded({extenden : true}));
 app.set("view engine", "ejs");
 
@@ -14,7 +26,10 @@ app.get("/", function(req, res){
 });
 
 app.get("/posts", function(req, res) {
-    res.render("posts");
+    var name = req.body.name;
+    var image = req.body.image;
+
+    res.render("posts", {posts:posts});
 });
 
 app.listen(PORT, function(){
